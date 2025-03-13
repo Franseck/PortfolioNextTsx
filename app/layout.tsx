2e0eac77f97c68fb74e18bd161ebd548dcc834d1
Seckin Ozturk <franseck102@gmail.com>
2025-03-13 13:50:15 -0400

    navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Special_Elite } from 'next/font/google'
 import './globals.css'
+import Navbar from '@/components/Navbar'
 
 const inter = Special_Elite({ weight: '400',
   preload: false,})
@@ -17,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <body className={inter.className}>
+      <Navbar />
+      {children}
+            </body>
+  </html>
   )
 }
